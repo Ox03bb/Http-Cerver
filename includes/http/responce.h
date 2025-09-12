9f9f4085c04864ef0bdc15b90b598d960ac6d2ed
Ox03bb <alibouakaz91@gmail.com>
2025-09-13 00:00:55 +0100

    add|return http responce , and dir readme file

---
+++ includes/http/responce.h
@@ -0,0 +1,7 @@
+#ifndef RESPONCE_H
+#define RESPONCE_H
+
+char* res_404(void);
+char* res_200(const char* body);
+
+#endif // RESPONCE_H
---
+++ includes/http/status_codes.h
@@ -0,0 +1,24 @@
+
+#define CRLF "\r\n"
+
+#define HTTP_100 "HTTP/1.1 100 Continue\r\n"
+#define HTTP_101 "HTTP/1.1 101 Switching Protocols\r\n"
+#define HTTP_102 "HTTP/1.1 102 Processing\r\n"
+
+#define HTTP_200 "HTTP/1.1 200 OK\r\n"
+#define HTTP_201 "HTTP/1.1 201 Created\r\n"
+#define HTTP_202 "HTTP/1.1 202 Accepted\r\n"
+#define HTTP_204 "HTTP/1.1 204 No Content\r\n"
+
+#define HTTP_301 "HTTP/1.1 301 Moved Permanently\r\n"
+#define HTTP_302 "HTTP/1.1 302 Found\r\n"
+#define HTTP_304 "HTTP/1.1 304 Not Modified\r\n"
+
+#define HTTP_400 "HTTP/1.1 400 Bad Request\r\n"
+#define HTTP_401 "HTTP/1.1 401 Unauthorized\r\n"
+#define HTTP_403 "HTTP/1.1 403 Forbidden\r\n"
+#define HTTP_404 "HTTP/1.1 404 Not Found\r\n"
+
+#define HTTP_500 "HTTP/1.1 500 Internal Server Error\r\n"
+#define HTTP_501 "HTTP/1.1 501 Not Implemented\r\n"
+#define HTTP_505 "HTTP/1.1 505 HTTP Version Not Supported\r\n"
